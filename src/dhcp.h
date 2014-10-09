89cf4d23257e03ce3b7f09e27bb2449857e2ce15
Michael Braun <michael-dev@fami-braun.de>
2014-10-09 11:20:29 +0200

    add doc

--- src/dhcp.h
+++ src/dhcp.h
@@ -31,12 +31,22 @@ typedef void (*updated_lease_cb)(const uint8_t* mac, const struct in_addr* yip, 
 typedef void (*lease_cb)(const struct in_addr* yip, const uint8_t* mac, const char* ifname, const uint32_t expiresAt);
 typedef void (*lease_lookup_by_mac_cb)(const char* ifname, const uint8_t* mac, lease_cb cb);
 
+/* check if mac is present locally (i.e. on wifi port) */
 void add_is_local_hook(is_local_cb cb);
 int is_local (const uint8_t* mac, const char* ifname);
+
+/* update_lease_hook: query some external source to update a locally configured lease.
+ * The callback is expected to call updated_lease with an appropiate reason if it modified the lease.
+ */
 void add_update_lease_hook(update_lease_cb cb);
 int update_lease(const char* ifname, const uint8_t* mac, const struct in_addr* ip, uint32_t* expiresAt);
+/* updated_lease_hook: called whenever the locally stored leases are modified. */
 void add_updated_lease_hook(updated_lease_cb cb);
 void updated_lease(const uint8_t* mac, const struct in_addr* yip, const char* ifname, const uint32_t expiresAt, const enum t_lease_update_src reason);
+/* lease_lookup_by_mac_hook: called whenever a lease needs to be looked up externally (i.e. if a mac is learned locally)
+ * the lookup function is passed a callback and it is expected to call the given callback with the data found.
+ */
 void add_lease_lookup_by_mac(lease_lookup_by_mac_cb cb);
+/* try to find the lease externally and call the given callback with the data found. */
 void lease_lookup_by_mac(const char* ifname, const uint8_t* mac, lease_cb cb);
 
