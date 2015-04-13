3bd94318dbb37524d66c156eb89b6e7f665e1d84
Michael Braun <michael-dev@fami-braun.de>
2015-04-13 18:03:31 +0200

    dnat-target RETURN was not accepted in -t nat

--- src/ebtables.c
+++ src/ebtables.c
@@ -57,7 +57,7 @@ void ebtables_add(const struct in_addr* yip, const uint8_t* mac, const char* ifn
 	         ether_ntoa_z((struct ether_addr *)mac), inet_ntoa(*yip), ifname);
 	ebtables_run(cmd);
 
-	snprintf(cmd, sizeof(cmd), EBTABLES " -t nat -A " CHAINNAME " --proto arp --arp-ip-dst %s --logical-in %s -j dnat --to-destination %s --dnat-target RETURN",
+	snprintf(cmd, sizeof(cmd), EBTABLES " -t nat -A " CHAINNAME " --proto arp --arp-ip-dst %s --logical-in %s -j dnat --to-destination %s --dnat-target CONTINUE",
 	         inet_ntoa(*yip), ifname, ether_ntoa_z((struct ether_addr *)mac));
 	ebtables_run(cmd);
 }
@@ -76,7 +76,7 @@ void ebtables_del(const struct in_addr* yip, const uint8_t* mac, const char* ifn
 	         ether_ntoa_z((struct ether_addr *)mac), inet_ntoa(*yip), ifname);
 	ebtables_run(cmd);
 
-	snprintf(cmd, sizeof(cmd), EBTABLES " -t nat -D " CHAINNAME " --proto arp --arp-ip-dst %s --logical-in %s -j dnat --to-destination %s --dnat-target RETURN",
+	snprintf(cmd, sizeof(cmd), EBTABLES " -t nat -D " CHAINNAME " --proto arp --arp-ip-dst %s --logical-in %s -j dnat --to-destination %s --dnat-target CONTINUE",
 	         inet_ntoa(*yip), ifname, ether_ntoa_z((struct ether_addr *)mac));
 	ebtables_run(cmd);
 }
